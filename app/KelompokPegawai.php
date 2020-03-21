fde4efada08b28f9095e88e2e31747ee0f526b12
tusfendi <tusfendi@gmail.com>
2020-03-21 17:15:59 +0700

    CRUD kelompok Kerja

---
+++ app/KelompokPegawai.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class KelompokPegawai extends Model
+{
+    protected $table = 'kelompok_pegawai';
+    protected $primaryKey = 'id_kelompok_pegawai';
+}
