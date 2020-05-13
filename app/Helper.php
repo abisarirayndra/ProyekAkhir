aeb2d120e795fddbf6110ad700073f525a00aa53
tusfendi <tusfendi@gmail.com>
2020-05-14 04:11:40 +0700

    Penambahan fungsi untuk konversi panjang string 1 digit menjadi 2 digit

--- app/Helper.php
+++ app/Helper.php
@@ -33,11 +33,30 @@ class Helper
             return Date("H:i:s",strtotime("$hasil"));
     }
 
+    /**
+     * Mengkonversi data berformat waktu menjadi string
+     *
+     * @param  $time
+     * @return ($hours * 60) + $minutes;
+     */
     public static function time_to_int($time){
         if($time != '0') {
             list($hours, $minutes) = explode(':', $time);
             return ($hours * 60) + $minutes;
         }
         return 0;
+    }
+
+    /**
+     * Mengkonversi nilai, Contoh : 1 menjadi 01
+     *
+     * @param  $id
+     * @return $id
+     */
+    public static function convert_id($id){
+        if( strlen($id) == 1 ){
+            return '0'.$id;
+        }else
+            return $id;
     }
 }
